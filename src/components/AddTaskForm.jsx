31fcc4b16e20fbe7aa71c67f7716acb2cb0baa0a
Max <verside@ya.ru>
2026-05-12 19:46:17 +0300

    Lesson 3

---
+++ src/components/AddTaskForm.jsx
@@ -0,0 +1,13 @@
+import Button from "./Button";
+import Field from "./FIeld";
+
+const AddTaskForm = () => {
+  return (
+    <form className="todo__form">
+      <Field />
+      <Button />
+    </form>
+  )
+}
+
+export default AddTaskForm;
