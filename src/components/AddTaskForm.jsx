f38d1c703fa6b62813e02e3fb2aec72208632c69
Max <verside@ya.ru>
2026-05-13 16:41:32 +0300

    Lesson 4

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -4,8 +4,12 @@ import Field from "./FIeld";
 const AddTaskForm = () => {
   return (
     <form className="todo__form">
-      <Field />
-      <Button />
+      <Field 
+        className="todo__field"
+        label="New task title"
+        id="new-task"
+      />
+      <Button type="submit">Add</Button>
     </form>
   )
 }
