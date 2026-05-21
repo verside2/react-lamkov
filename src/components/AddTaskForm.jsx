3b26002b39298b98aae3bb7bc65d03ea59746779
Max <verside@ya.ru>
2026-05-21 19:24:28 +0300

    Lesson 12

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Button from "./Button";
 import Field from "./FIeld";
 import { TasksContext } from "../context/TasksContext";
@@ -11,10 +11,26 @@ const AddTaskForm = () => {
     newTaskInputRef,
   } = useContext(TasksContext);
 
+  const [error, setError] = useState("");
+
+  const clearNewTaskTitle = newTaskTitle.trim();
+  const isNewTaskTitleEmpty = clearNewTaskTitle.length === 0;
+
   const onSubmit = (event) => {
     event.preventDefault();
 
-    addTask();
+    if (!isNewTaskTitleEmpty) {
+      addTask(clearNewTaskTitle);
+    }
+  }
+
+  const onInput = (event) => {
+    const {value} = event.target;
+    const clearValue = value.trim();
+    const hasOnlySpaces = value.length > 0 && clearValue.length === 0;
+  
+    setNewTaskTitle(value);
+    setError(hasOnlySpaces ? "Задача не может быть пустая" : "");
   }
 
   return (
@@ -23,11 +39,17 @@ const AddTaskForm = () => {
         className="todo__field"
         label="New task title"
         id="new-task"
+        error={error}
         value={newTaskTitle}
-        onInput={(event) => setNewTaskTitle(event.target.value)}
+        onInput={onInput}
         ref={newTaskInputRef}
       />
-      <Button type="submit">Add</Button>
+      <Button 
+        type="submit"
+        isDisabled={isNewTaskTitleEmpty}
+      >
+        Add
+      </Button>
     </form>
   )
 }
--- src/components/FIeld.jsx
+++ src/components/FIeld.jsx
@@ -6,6 +6,7 @@ const Field = (props) => {
     type = "text",
     onInput,
     value,
+    error,
     ref,
   } = props;
 
@@ -18,7 +19,7 @@ const Field = (props) => {
         {label}
       </label>
       <input
-        className="field__input"
+        className={`field__input ${error ? "is-invalid" : ""}`}
         id={id}
         placeholder=" "
         autoComplete="off"
@@ -27,6 +28,9 @@ const Field = (props) => {
         onInput={onInput}
         ref={ref}
       />
+    {error && (
+      <span className="field__error" title={error}>{error}</span>
+    )}
     </div>
   )
 }
